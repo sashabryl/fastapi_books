c2fbf05577b5069accf39cad5fac8a4ece80041e
Olexander Bryl <edlrian814@gmail.com>
2024-08-11 22:55:08 +0300

    Add create_book endpoint

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+import schemas
+import crud
+from dependencies import get_db
+
+app = FastAPI(root_path="/api")
 
 
-app = FastAPI()
+@app.post("/books/", response_model=schemas.Book)
+def create_book(
+    book_schema: Annotated[schemas.BookBase, Depends()],
+    db: Session = Depends(get_db)
+) -> schemas.Book:
+    return crud.create_book(book_schema, db)
