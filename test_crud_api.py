f21fd3958ab97dbab77420c796cc1a01597532b5
Olexander Bryl <edlrian814@gmail.com>
2024-08-12 00:30:18 +0300

    Test retrieving books

--- test_crud_api.py
+++ test_crud_api.py
@@ -15,10 +15,33 @@ def test_create_book(test_client, book_payload):
 
 def test_create_book_wrong_publication_year(test_client, book_payload):
     book_payload["publication_year"] = -2025
-    response = test_client.post("/api/books/", json = book_payload)
+    response = test_client.post("/api/books/", json=book_payload)
     assert response.status_code == 422
 
     book_payload["publication_year"] = 0
     response = test_client.post("/api/books/", json=book_payload)
     assert response.status_code == 422
 
+
+def test_get_book_by_id(test_client, book_payload):
+    test_client.post("/api/books/", json=book_payload)
+    response = test_client.get("/api/books/1/")
+    assert response.status_code == 200
+    assert response.json()["author"] == "Me"
+
+    response = test_client.get("/api/books/2/")
+    assert response.status_code == 404
+
+
+def test_get_books(test_client, book_payload):
+    test_client.post("/api/books/", json=book_payload)
+    book_payload["title"] = "A book 2"
+    test_client.post("/api/books/", json=book_payload)
+
+    response = test_client.get("/api/books/")
+    assert response.status_code == 200
+
+    response_json = response.json()
+    assert len(response_json) == 2
+    assert response_json[0]["title"] == "A book" if response_json[0]["id"] == 1 else "A book 2"
+    assert response_json[1]["title"] == "A book 2" if response_json[1]["id"] == 2 else "A book"
