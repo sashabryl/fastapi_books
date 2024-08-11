cbbfcae97c9821824203f323b95ca37fd580d536
Olexander Bryl <edlrian814@gmail.com>
2024-08-12 00:35:07 +0300

    Test book updation

--- test_crud_api.py
+++ test_crud_api.py
@@ -45,3 +45,19 @@ def test_get_books(test_client, book_payload):
     assert len(response_json) == 2
     assert response_json[0]["title"] == "A book" if response_json[0]["id"] == 1 else "A book 2"
     assert response_json[1]["title"] == "A book 2" if response_json[1]["id"] == 2 else "A book"
+
+
+def test_updating_books(test_client, book_payload):
+    test_client.post("/api/books/", json=book_payload)
+    book_payload["title"] = "A novel"
+
+    response = test_client.put("/api/books/1/", json=book_payload)
+    assert response.status_code == 200
+    assert response.json()["title"] == "A novel"
+
+    response = test_client.put("/api/books/2/", json=book_payload)
+    assert response.status_code == 404
+
+    book_payload["publication_year"] = 0
+    response = test_client.put("/api/books/1/", json=book_payload)
+    assert response.status_code == 422
